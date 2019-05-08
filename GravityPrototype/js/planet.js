c20bb096848e79d5ee3fc4494aa07490eb65c29f
Rachel Haub <rachelhaub@gmail.com>
2019-05-07 17:45:04 -0700

    Adjusted distance calculations
    Orbit is now at a variable height

--- GravityPrototype/js/planet.js
+++ GravityPrototype/js/planet.js
@@ -15,6 +15,8 @@ function Planet(game, xPos, yPos, key, player, acceleration, maxVel) {
 	this.MAX_VELOCITY = maxVel;
 	this.ACCELERATION = acceleration;
 
+
+
 }
 
 Planet.prototype = Object.create(Phaser.Sprite.prototype);
@@ -23,7 +25,7 @@ Planet.prototype.constructor = Planet;
 Planet.prototype.update = function() {
 	distance = Phaser.Math.distance(this.playerRef.position.x, this.playerRef.position.y, this.position.x, this.position.y);
 
-	if(distance <= 300 && distance >200)
+	/*if(distance <= 300 && distance >200)
 	{
 		//game.physics.arcade.accelerateToObject(this.playerRef, this, this.ACCELERATION, this.MAX_VELOCITY/5, this.MAX_VELOCITY/5);
 		if(this.playerRef.position.x <= this.position.x)
@@ -43,8 +45,8 @@ Planet.prototype.update = function() {
 		{
 			this.playerRef.position.y -=2;
 		}
-	}
-	if(distance <= 200)
+	}*/
+	if(distance <= 200 && !game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
 	{
 
 		//this.playerRef.pivot.x = this.position.x;
@@ -54,15 +56,28 @@ Planet.prototype.update = function() {
 		//console.log('y: ' + this.playerRef.pivot.y);
 
 		//this.playerRef.rotation +=.025;
-		Phaser.Point.rotate(this.playerRef, this.position.x, this.position.y, .025, false, 150);
-		if(game.input.keyboard.isDown(Phaser.Keyboard.SPACE))
-		{
-			//accelerate out of orbit
-		}
+		Phaser.Point.rotate(this.playerRef, this.position.x, this.position.y, .025, false,  Math.sqrt(((this.x - this.playerRef.x) * (this.x - this.playerRef.x)) + ((this.y - this.playerRef.y) * (this.y - this.playerRef.y))));
+
+		this.playerRef.acceleration = new Phaser.Point(0,0);
+		this.playerRef.velocity = new Phaser.Point(0,0);
+		
 	}
 	else 
 	{
 		//this.playerRef.body.acceleration = 0;
 	}
 
+	if(game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
+		{
+			//this.playerRef.acceleration = new Phaser.Point(-(this.playerRef.position.x-this.position.x), -(this.playerRef.position.y-this.position.y));
+			
+			game.physics.arcade.accelerateToObject(this.playerRef, new Phaser.Point(-(this.playerRef.position.x-this.position.x), -(this.playerRef.position.y-this.position.y)));
+
+			var pointTime = new Phaser.Point(-(this.playerRef.position.x-this.position.x), -(this.playerRef.position.y-this.position.y));
+
+			
+		}
+
+		console.log('X: ' + this.playerRef.position.x);
+			console.log('Y: ' + this.playerRef.position.y);
 }
