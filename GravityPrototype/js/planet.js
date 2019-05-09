2b370a5750f74e31dc580de0f3f2e26f10234d9e
Rachel Haub <rachelhaub@gmail.com>
2019-05-09 14:23:11 -0700

    Adjusted to feel better

--- GravityPrototype/js/planet.js
+++ GravityPrototype/js/planet.js
@@ -14,9 +14,6 @@ function Planet(game, xPos, yPos, key, player, acceleration, maxVel) {
 	//"constants"
 	this.MAX_VELOCITY = maxVel;
 	this.ACCELERATION = acceleration;
-
-
-
 }
 
 Planet.prototype = Object.create(Phaser.Sprite.prototype);
@@ -25,32 +22,39 @@ Planet.prototype.constructor = Planet;
 Planet.prototype.update = function() {
 	distance = Phaser.Math.distance(this.playerRef.position.x, this.playerRef.position.y, this.position.x, this.position.y);
 
-	if(distance <= 200 && !game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
+	if(distance <= 200)
 	{
+		if(!game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
+		{
 
-		this.radius = Math.sqrt(((this.x - this.playerRef.x) * (this.x - this.playerRef.x)) + ((this.y - this.playerRef.y) * (this.y - this.playerRef.y)));
+			this.radius = Math.sqrt(((this.x - this.playerRef.x) * (this.x - this.playerRef.x)) + ((this.y - this.playerRef.y) * (this.y - this.playerRef.y)));
 
-		Phaser.Point.rotate(this.playerRef, this.position.x, this.position.y, .025, false, this.radius);
+			Phaser.Point.rotate(this.playerRef, this.position.x, this.position.y, .025, false, this.radius);
 
-	}
+		}
 
-	angle = Phaser.Point.angle(this.playerRef.position, this.position);
+		angle = Phaser.Point.angle(this.playerRef.position, this.position);
 
-	if(game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR))
-	{
-		this.playerRef.body.velocity.x = Math.cos(angle) * 200;
-		this.playerRef.body.velocity.y = Math.sin(angle) * 200;
+		if(game.input.keyboard.justPressed(Phaser.Keyboard.SPACEBAR))
+		{
+			this.playerRef.body.velocity.x = Math.cos(angle) * 200;
+			this.playerRef.body.velocity.y = Math.sin(angle) * 200;
 
-		force =  (1000 - distance) / 200;
+			force =  (1000 - distance) / 200;
 			
-		this.playerRef.body.velocity.x -= Math.cos(angle) * force;
-		this.playerRef.body.velocity.y-= Math.sin(angle) * force;
+			this.playerRef.body.velocity.x -= Math.cos(angle) * force;
+			this.playerRef.body.velocity.y-= Math.sin(angle) * force;
+		}
+		else
+		{
+			this.playerRef.body.acceleration = new Phaser.Point(0,0);
+			//this.playerRef.body.velocity = new Phaser.Point(0,0);
+		}
 	}
-	else
-	{
-		this.playerRef.acceleration = new Phaser.Point(0,0);
-		this.playerRef.velocity = new Phaser.Point(0,0);
-	}
+
+
+
+	
 
 
 }
