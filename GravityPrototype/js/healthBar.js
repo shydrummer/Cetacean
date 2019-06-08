04a4006c45ca34753e18e5a19ba5c52047d05b81
Rachel Haub <rachelhaub@gmail.com>
2019-06-07 17:51:47 -0700

    Added new Star asset

--- GravityPrototype/js/healthBar.js
+++ GravityPrototype/js/healthBar.js
@@ -1,8 +1,10 @@
 function HealthBar(game, xPos, yPos, key) {
-	this.heath1 = game.add.sprite(xPos, yPos, key);
+	this.health1 = game.add.sprite(xPos, yPos, key);
+	this.health1.fixedToCamera = true;
 	this.health2 = game.add.sprite(xPos + this.health1.width + 20, yPos, key);
+	this.health2.fixedToCamera = true;
 	this.health3 = game.add.sprite(xPos + (this.health1.width*2) + 20, yPos, key);
-
+	this.health3.fixedToCamera = true;
 }
 
 HealthBar.prototype = Object.create(Phaser.Sprite.prototype);
@@ -17,7 +19,7 @@ function decreaseHealth()
 	if(this.health2 == null)
 	{
 		this.health1.destroy();
-		return -1;
+		//return -1;
 	}
 	else if (this.health3 == null)
 	{
